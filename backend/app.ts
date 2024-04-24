1d8f3beeedb6111ffd53ade289c6ded51b68947f
Joe Del Balzo <jdelbalzo99@gmail.com>
2024-04-24 14:03:38 -0400

    req

--- backend/app.ts
+++ backend/app.ts
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
 app.use(
   "/api/coupon-codes",
